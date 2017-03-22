73cdb424d05470545cde865cb13d6f1d63eb16b8
Ricky Chien <ricky060709@gmail.com>
2017-03-22 13:37:13 +0800

    Update configs

--- bin/getConfig.js
+++ bin/getConfig.js
@@ -1,1 +1,15 @@
-module.exports = () => require("../configs/development.json");
+const fs = require("fs");
+const path = require("path");
+
+function getConfig() {
+  const developmentConfig = require("../configs/development.json");
+
+  let localConfig = {};
+  if (fs.existsSync(path.resolve(__dirname, "../configs/local.json"))) {
+    localConfig = require("../configs/local.json");
+  }
+
+  return Object.assign({},developmentConfig, localConfig);
+}
+
+module.exports = getConfig;
